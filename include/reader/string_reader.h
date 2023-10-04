f33387bbe729d7ba00deb49e852200f377d6d3be
sapcerk <sapcerk@gmail.com>
2023-10-04 16:09:47 +0300

    feat(reader): string reader implementation

---
+++ include/reader/string_reader.h
@@ -0,0 +1,27 @@
+#ifndef YALPCONF_SRC_READER_STRING_READER_H_
+#define YALPCONF_SRC_READER_STRING_READER_H_
+
+#include <string>
+
+#include "error.h"
+#include "reader/reader.h"
+
+namespace yalpconf {
+
+class StringReader : public Reader {
+  public:
+    StringReader(const std::string& input_str);
+    StringReader(std::string&& input_str);
+    ~StringReader() = default;
+    Error ReadConfiguration() override;
+    bool empty() const override;
+    char TakeNextSymbol() const override;
+    char CheckNextSymbol() const override;
+  private:
+    const std::string input_str_;
+    mutable size_t next_symbol_position_;
+};
+
+} // namespace yalpconf
+
+#endif // YALPCONF_SRC_READER_STRING_READER_H_
---
+++ src/reader/string_reader.cc
@@ -0,0 +1,31 @@
+#include <string>
+
+#include "error.h"
+#include "reader/reader.h"
+#include "reader/string_reader.h"
+
+namespace yalpconf {
+
+StringReader::StringReader(const std::string& input_str) 
+    : input_str_ { input_str }, next_symbol_position_ { 0 } {};
+
+StringReader::StringReader(std::string&& input_str) 
+    : input_str_ { std::move(input_str) }, next_symbol_position_ { 0 } {};
+  
+Error StringReader::ReadConfiguration() {
+  return Error(ErrorCode::None);
+}
+
+bool StringReader::empty() const {
+  return next_symbol_position_ >= input_str_.size();
+}
+
+char StringReader::TakeNextSymbol() const {
+  return input_str_[next_symbol_position_++];
+}
+
+char StringReader::CheckNextSymbol() const {
+  return input_str_[next_symbol_position_];
+}
+
+} // namespace yalpconf
