67dc00629bbc69bbbfa719d0a731ef7609121944
sapcerk <sapcerk@gmail.com>
2023-10-04 15:59:33 +0300

    feat(reader): reader interface implementation

--- include/reader/reader.h
+++ include/reader/reader.h
@@ -1,14 +1,17 @@
 #ifndef YALPCONF_SRC_READER_READER_H_
 #define YALPCONF_SRC_READER_READER_H_
 
-#include "reader/buffer.h"
+#include "error.h"
 
 namespace yalpconf {
 
 class Reader {
   public:
     virtual ~Reader() = default;
-    virtual Buffer ReadConfiguration() = 0;
+    virtual Error ReadConfiguration() = 0;
+    virtual bool empty() const = 0;
+    virtual char TakeNextSymbol() const = 0;
+    virtual char CheckNextSymbol() const = 0;
 };
 
 } // namespace yalpconf
