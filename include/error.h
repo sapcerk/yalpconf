3c65930337603da76cfb8c85728b5c3eace46a00
sapcerk <sapcerk@gmail.com>
2023-10-12 18:50:48 +0300

    feat: add expected string and Parser errors

--- include/error.h
+++ include/error.h
@@ -1,5 +1,8 @@
-#ifndef YALPCONF_SRC_ERROR_H_
-#define YALPCONF_SRC_ERROR_H_
+#ifndef YALPCONF_INCLUDE_ERROR_H_
+#define YALPCONF_INCLUDE_ERROR_H_
+
+#include <string>
+#include <unordered_map>
 
 namespace yalpconf {
 
@@ -7,6 +10,11 @@ enum class ErrorCode : int {
   None = 0,
   CantOpenFile,
   UnexpectedSymbol,
+  ParserReduce,
+  ParserShift,
+  ParserGoto,
+  ParserUnexpectedToken,
+  ParserAlreadyInScope,
 };
 
 class Error {
@@ -16,12 +24,15 @@ class Error {
     explicit Error(ErrorCode code, size_t line);
     explicit Error(ErrorCode code, size_t line, std::string near_string);
     explicit Error(ErrorCode code, size_t line, char near_string);
+    explicit Error(ErrorCode code, size_t line, 
+        std::string near_string, std::string expected_string);
     Error(const Error& other);
     Error(Error&& other) noexcept;
     ErrorCode GetCode() const;
     int GetCodeInt() const;
     size_t GetLine() const;
     std::string GetNearString() const;
+    std::string GetExpectedString() const;
     std::string GetMessage() const;
     Error& operator=(const Error& other);
     Error& operator=(Error&& other) noexcept;
@@ -29,8 +40,20 @@ class Error {
     ErrorCode code_;
     size_t line_;
     std::string near_string_;
+    std::string expected_string_;
+    static const std::unordered_map<ErrorCode, std::string> message_;
+};
+
+inline const std::unordered_map<ErrorCode, std::string> Error::message_ {
+  { ErrorCode::CantOpenFile, "Unable to open file." },
+  { ErrorCode::UnexpectedSymbol, "The lexical analyzer detected an unexpected symbol." },
+  { ErrorCode::ParserReduce, "Parser unable to perform reduce action." },
+  { ErrorCode::ParserShift, "Parser unable to perform shift action." },
+  { ErrorCode::ParserGoto, "Parser unable to perform goto action." },
+  { ErrorCode::ParserUnexpectedToken, "Parser detected an unexpected item." },
+  { ErrorCode::ParserAlreadyInScope, "Setting with the same name already exists in scope." },
 };
 
 } // namespace yalpconf
 
-#endif // YALPCONF_SRC_ERROR_H_
+#endif // YALPCONF_INCLUDE_ERROR_H_
--- src/error.cc
+++ src/error.cc
@@ -17,21 +17,30 @@ Error::Error(ErrorCode code, size_t line, std::string near_string)
     : code_ { code }, line_ { line }, near_string_ { near_string } {}
 
 Error::Error(ErrorCode code, size_t line, char near_string)
-    : code_ { code }, line_ { line }, near_string_ { std::string(1, near_string) } {}
+    : code_ { code }, line_ { line }, 
+      near_string_ { std::string(1, near_string) } {}
+
+Error::Error(ErrorCode code, size_t line, 
+    std::string near_string, std::string expected_string)
+    : code_ { code }, line_ { line }, 
+      near_string_ { near_string }, expected_string_ { expected_string } {}
 
 Error::Error(const Error& other) {
   code_ = other.code_;
   line_ = other.line_;
   near_string_ = other.near_string_;
+  expected_string_ = other.expected_string_;
 }
 
 Error::Error(Error&& other) noexcept {
   code_ = std::move(other.code_);
   line_ = std::move(other.line_);
   near_string_ = std::move(other.near_string_);
+  expected_string_ = std::move(other.expected_string_);
   other.code_ = ErrorCode::None;
   other.line_ = 0;
   other.near_string_ = std::string();
+  other.expected_string_ = std::string();
 }
 
 ErrorCode Error::GetCode() const {
@@ -50,7 +59,14 @@ std::string Error::GetNearString() const {
   return near_string_;
 }
 
+std::string Error::GetExpectedString() const {
+  return expected_string_;
+}
+
 std::string Error::GetMessage() const {
+  return ("Error occurred on line: " + std::to_string(line_) +
+      ". Near string: " + near_string_ + ". Expected: " + expected_string_ +
+      ". Desctiption: " + message_.at(code_));
 }
 
 Error& Error::operator=(const Error& other) {
@@ -58,6 +74,7 @@ Error& Error::operator=(const Error& other) {
     code_ = other.code_;
     line_ = other.line_;
     near_string_ = other.near_string_;
+    expected_string_ = other.expected_string_;
   }
   return *this;
 }
@@ -67,9 +84,11 @@ Error& Error::operator=(Error&& other) noexcept {
     code_ = std::move(other.code_);
     line_ = std::move(other.line_);
     near_string_ = std::move(other.near_string_);
+    expected_string_ = std::move(other.expected_string_);
     other.code_ = ErrorCode::None;
     other.line_ = 0;
     other.near_string_ = std::string();
+    other.expected_string_ = std::string();
   }
   return *this;
 }
